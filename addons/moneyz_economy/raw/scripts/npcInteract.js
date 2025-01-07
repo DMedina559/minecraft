0f95593ee88937270752af0b9acf97da2742ec25
Danny <68480549+DMedina559@users.noreply.github.com>
2025-01-07 10:06:40 -0800

    Open JS menu when NPC name matches

---
+++ addons/moneyz_economy/raw/scripts/npcInteract.js
@@ -0,0 +1,64 @@
+import { world, system } from "@minecraft/server";
+import { customShop } from "./gui/custom_shop.js";
+import { openRewardsMenu } from "./gui/rewards_menu.js";
+import { luckyPurchase } from './gui/lucky_purchase.js';
+import { testYourLuck } from "./games/randomNum.js";
+import { start21Game } from "./games/21Game.js";
+import { startCrapsGame } from "./games/diceGame.js";
+import { startSlotsGame } from "./games/slotGame.js";
+import { log, LOG_LEVELS } from "./logger.js";
+
+// Open JS Menu when Player interact with NPC that matches the required Name
+world.beforeEvents.playerInteractWithEntity.subscribe((data) => {
+    const player = data.player;
+    const targetEntity = data.target;
+
+    if (!player || !targetEntity) {
+        log("Player or target entity is undefined. Unable to handle interaction.", LOG_LEVELS.ERROR);
+        return;
+    }
+
+    const npcCustomShop = world.getDynamicProperty("customShop") || "Custom Shop";
+    const npcRewards = world.getDynamicProperty("npcRewards") || "Daily Rewards";
+    const npcLuckyP = world.getDynamicProperty("npcLuckyP") || "Lucky Purchase";
+    const npc21Game = world.getDynamicProperty("npc21") || "21";
+    const npcTestLuck = world.getDynamicProperty("npcTestLuck") || "Test Luck";
+    const npcDiceGame = world.getDynamicProperty("npcDice") || "Dice";
+    const npcSlotsGame = world.getDynamicProperty("npcSlots") || "Slots";
+
+    const npcName = targetEntity.nameTag ? targetEntity.nameTag : "Unnamed NPC";
+    log(`Interacted with NPC: ${npcName}`, LOG_LEVELS.INFO);
+
+    if (targetEntity.typeId === "minecraft:npc") {
+        log(`Player object: ${JSON.stringify(player)}`, LOG_LEVELS.DEBUG);
+        log(`Target entity object: ${JSON.stringify(targetEntity)}`, LOG_LEVELS.DEBUG);
+
+        // Open corresponding menu based on NPC name
+        if (npcName === npcCustomShop) {
+            data.cancel = true;
+            system.run(() => customShop(player));
+        } else if (npcName === npcRewards) {
+            data.cancel = true;
+            system.run(() => openRewardsMenu(player));
+        } else if (npcName === npcLuckyP) {
+            data.cancel = true;
+            system.run(() => luckyPurchase(player));
+        } else if (npcName === npc21Game) {
+            data.cancel = true;
+            system.run(() => start21Game(player));
+        } else if (npcName === npcTestLuck) {
+            data.cancel = true;
+            system.run(() => testYourLuck(player));
+        } else if (npcName === npcDiceGame) {
+            data.cancel = true;
+            system.run(() => startCrapsGame(player));
+        } else if (npcName === npcSlotsGame) {
+            data.cancel = true;
+            system.run(() => startSlotsGame(player));
+        } else {
+        
+        }
+    } else {
+    
+    }
+});
