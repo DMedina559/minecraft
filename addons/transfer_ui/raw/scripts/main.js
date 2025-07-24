98c924565881d273bede26343a7795edd1045daa
Danny <68480549+DMedina559@users.noreply.github.com>
2025-07-23 18:44:13 -0700

    fix: transfer ui addon

--- addons/transfer_ui/raw/scripts/main.js
+++ addons/transfer_ui/raw/scripts/main.js
@@ -3,16 +3,15 @@ import { ModalFormData } from "@minecraft/server-ui";
 import { transferPlayer } from "@minecraft/server-admin";
 
 world.beforeEvents.itemUse.subscribe(data => {
-    const player = data.source
-    if (data.itemStack.typeId == "minecraft:compass") system.run(() => showServerTransferMenu(player))
-
+    const player = data.source;
+    if (data.itemStack.typeId == "minecraft:compass") system.run(() => showServerTransferMenu(player));
 });
 
 function showServerTransferMenu(player) {
     const form = new ModalFormData()
         .title("§l§fServer Transfer")
         .textField("§fEnter Server IP:", "e.g., 192.168.0.1")
-        .textField("§fEnter Server Port:", "e.g., 19132")
+        .textField("§fEnter Server Port:", "e.g., 19132");
 
     form.show(player).then((response) => {
         if (!response.canceled) {
@@ -24,12 +23,13 @@ function showServerTransferMenu(player) {
             }
 
             try {
-                transferPlayer(player, serverIp, parseInt(serverPort));
-                player.sendMessage(`§aSending to ${serverIp}:${serverPort}`);
-                console.log(`Sending Player ${player} to ${serverIp}:${serverPort}`);
+                // Using the TransferPlayerIpPortOptions interface for transferPlayer
+                transferPlayer(player, { hostname: serverIp, port: parseInt(serverPort) });
+                player.sendMessage(`§aSending to ${ serverIp }:${ serverPort } `);
+                console.log(`Sending Player ${ player.name } to ${ serverIp }:${ parseInt(serverPort) } `);
             } catch (error) {
-                player.sendMessage(`§cFailed to transfer: ${error.message}`);
-                console.error(`Error transferring player: ${error}`);
+                player.sendMessage(`§cFailed to transfer: ${ error.message } `);
+                console.error(`Error transferring player: ${ error } `);
             }
         }
     });
